a89bb0fcc37b57fddfadaedf0497734d57214fe9
Jerry She <173936744+jerrysxy@users.noreply.github.com>
2026-08-29 16:01:26 +0800

    Add: python-list-initialization - submission-0

---
+++ Python Coding Interviews/python-list-initialization/submission-0.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+def create_list_with_value(size: int, index: int, value: int) -> List[int]:
+    create_list = [0] * size
+    #make a list with size "size"
+    #everything should be 0
+    #except for index, the middle one. Need to be = value. 
+    #now create_list = [0, 0, 0, 0, 0] up till size
+    #it will take input (size,index,value)
+    for i in create_list:
+        create_list[index] = value
+    return create_list
+
+
+# do not modify below this line
+print(create_list_with_value(5, 3, 7))
+print(create_list_with_value(1, 0, 5))
+print(create_list_with_value(10, 9, 9))
+print(create_list_with_value(10, 9, 0))
